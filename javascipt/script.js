2b206da99a9d83e3d99ed6a26ec76ba924c97c1e
MoNKeY-D-LuFFy-WTFm <yakubovm533@gmail.com>
2024-04-19 21:59:31 +0500

    first commit

---
+++ javascipt/script.js
@@ -0,0 +1,15 @@
+"use strict";
+// let n: number = 4;
+// let str: string = 'hi';
+// let ok: string = n + str;
+// console.log(ok);
+// let b1: boolean = 10 >= 10;
+// console.log(b1);
+// b1 = false;
+// console.log(b1);
+const n1 = 999;
+const s1 = 'Hello typeScript';
+document.querySelector('.out-1').textContent = String(n1);
+const num = Number(document.querySelector('.out-2').textContent);
+console.log(num);
+//# sourceMappingURL=script.js.map
---
+++ script.ts
@@ -0,0 +1,15 @@
+// let n: number = 4;
+// let str: string = 'hi';
+// let ok: string = n + str;
+// console.log(ok);
+// let b1: boolean = 10 >= 10;
+// console.log(b1);
+
+// b1 = false;
+// console.log(b1);
+const n1: number = 999;
+const s1: string = 'Hello typeScript';
+document.querySelector('.out-1').textContent = String(n1);
+
+const num: number = Number(document.querySelector('.out-2').textContent);
+console.log(num);
