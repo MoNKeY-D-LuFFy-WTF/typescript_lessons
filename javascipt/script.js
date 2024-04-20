ac38497142c3d587e9bd488815c83b51f49f1733
MoNKeY-D-LuFFy-WTFm <yakubovm533@gmail.com>
2024-04-20 13:36:57 +0500

    add function lesson

--- javascipt/script.js
+++ javascipt/script.js
@@ -1,32 +1,69 @@
 "use strict";
 //При выборе через класс querySelector('.name__class') нужно указать html element 
-const a1 = 'Hello typeScript';
-document.querySelector('.out-1').textContent = a1;
-const a2 = document.querySelector('.out-2').textContent;
-console.log(a2);
-const a3 = +document.querySelector('.out-3').textContent;
-console.log(a3);
+// const a1: string = 'Hello typeScript';
+// document.querySelector('.out-1').textContent = a1;
+// const a2: string = document.querySelector('.out-2').textContent;
+// console.log(a2);
+// const a3: number = +document.querySelector('.out-3').textContent;
+// console.log(a3);
 //получаем value из input HTMLInputElement
-const i5 = document.querySelector('.i-5');
-const hi = i5.value;
-console.log(hi);
+// const i5 = <HTMLInputElement>document.querySelector('.i-5');
+// const hi: string = i5.value
+// console.log(hi);
 // number
-const i6 = document.querySelector('.i-6');
-i6.value = String(88);
-console.log(+i6.value);
+// const i6 = document.querySelector('.i-6') as HTMLInputElement;
+// i6.value = String(88);
+// console.log(+i6.value);
 //checkbox
-const checkbox = document.querySelector('input[type="checkbox"]');
-if (checkbox.checked) {
-    console.log('checked');
-}
-else {
-    console.log('no checked');
-}
+// const checkbox = <HTMLInputElement>document.querySelector('input[type="checkbox"]');
+// if (checkbox.checked) {
+//     console.log('checked');
+// } else {
+//     console.log('no checked');
+// }
 //radio
-const radio = document.querySelector('input[name="r"]:checked');
-console.log(radio.value);
+// const radio = <HTMLInputElement>document.querySelector('input[name="r"]:checked');
+// console.log(radio.value);
 //select 
-const select = document.querySelector('.s-1');
-select.value = 'aaa';
-console.log(select.value);
+// const select = document.querySelector('.s-1') as HTMLSelectElement;
+// select.value = 'aaa';
+// console.log(select.value);
+//Функции TypeScript
+function t01() {
+    console.log(1);
+}
+//Возвращаем тип данных
+function t02() {
+    const n = 66;
+    return n;
+}
+console.log(t02());
+//  Процедура
+function t03() {
+    document.querySelector('.out-1').textContent = 'hello';
+}
+t03();
+//Аргументы 
+function t04(a, b) {
+    return a * b;
+}
+console.log(t04(4, 5));
+//Процедура аргумента 
+function t05(a, b) {
+    console.log(a * b);
+}
+t05(5, 6);
+//количество аргументов и необязательное 
+function t06(a, b) {
+    return a * b;
+}
+console.log(t06(10));
+//Несколько типов значений
+function checkEmail(email) {
+    //проверить есть ли @
+    if (email.indexOf('@') !== -1)
+        return email.toLowerCase();
+    return false;
+}
+console.log(checkEmail('harry@Potter.ua'));
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,39 +1,84 @@
 //При выборе через класс querySelector('.name__class') нужно указать html element 
 
-const a1: string = 'Hello typeScript';
-document.querySelector('.out-1').textContent = a1;
+// const a1: string = 'Hello typeScript';
+// document.querySelector('.out-1').textContent = a1;
 
-const a2: string = document.querySelector('.out-2').textContent;
-console.log(a2);
+// const a2: string = document.querySelector('.out-2').textContent;
+// console.log(a2);
 
-const a3: number = +document.querySelector('.out-3').textContent;
-console.log(a3);
+// const a3: number = +document.querySelector('.out-3').textContent;
+// console.log(a3);
 
 
 //получаем value из input HTMLInputElement
-const i5 = <HTMLInputElement>document.querySelector('.i-5');
-const hi: string = i5.value
-console.log(hi);
+// const i5 = <HTMLInputElement>document.querySelector('.i-5');
+// const hi: string = i5.value
+// console.log(hi);
 
 // number
-const i6 = document.querySelector('.i-6') as HTMLInputElement;
-i6.value = String(88);
-console.log(+i6.value);
+// const i6 = document.querySelector('.i-6') as HTMLInputElement;
+// i6.value = String(88);
+// console.log(+i6.value);
 
 //checkbox
-const checkbox = <HTMLInputElement>document.querySelector('input[type="checkbox"]');
-if (checkbox.checked) {
-    console.log('checked');
-} else {
-    console.log('no checked');
-}
+// const checkbox = <HTMLInputElement>document.querySelector('input[type="checkbox"]');
+// if (checkbox.checked) {
+//     console.log('checked');
+// } else {
+//     console.log('no checked');
+// }
 
 //radio
-const radio = <HTMLInputElement>document.querySelector('input[name="r"]:checked');
-console.log(radio.value);
+// const radio = <HTMLInputElement>document.querySelector('input[name="r"]:checked');
+// console.log(radio.value);
 
 
 //select 
-const select = document.querySelector('.s-1') as HTMLSelectElement;
-select.value = 'aaa';
-console.log(select.value);
+// const select = document.querySelector('.s-1') as HTMLSelectElement;
+// select.value = 'aaa';
+// console.log(select.value);
+
+
+//Функции TypeScript
+function t01() {
+    console.log(1);
+}
+//Возвращаем тип данных
+function t02(): number {
+    const n = 66;
+    return n;
+}
+console.log(t02());
+
+//  Процедура
+function t03(): void {
+    document.querySelector('.out-1').textContent = 'hello';
+}
+t03();
+
+//Аргументы 
+function t04(a: number, b: number): number {
+    return a * b;
+}
+console.log(t04(4, 5));
+
+//Процедура аргумента 
+function t05(a: number, b: number): void {
+    console.log(a * b);
+}
+t05(5, 6);
+
+//количество аргументов и необязательное 
+function t06(a: number, b?: number): number {
+    return a * b;
+}
+console.log(t06(10));
+
+//Несколько типов значений
+function checkEmail(email: string): string | boolean {
+    //проверить есть ли @
+    if (email.indexOf('@') !== -1) return email.toLowerCase()
+    return false;
+}
+console.log(checkEmail('harry@Potter.ua'));
+
