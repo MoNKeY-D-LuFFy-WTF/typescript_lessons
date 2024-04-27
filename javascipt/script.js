51f9943c9c5ef89e4139dfca0a38b9af72748876
MoNKeY-D-LuFFy-WTFm <yakubovm533@gmail.com>
2024-04-27 16:38:07 +0500

    add lesson class from typeScript

--- javascipt/script.js
+++ javascipt/script.js
@@ -1,34 +1,83 @@
 "use strict";
-const user = {
-    name: 'pupkin',
-    email: 'pupkin@gmail.com'
-};
-console.log(user);
+// const user: { name: string, email: string } = {
+//     name: 'pupkin',
+//     email: 'pupkin@gmail.com'
+// }
+// console.log(user);
 //Необязательные свойства
-const user2 = {
-    name: 'Oleg',
-    email: 'oleg44@gmail.com',
-    // age: 28,
-};
-console.log(user2);
+// const user2: { name: string, email: string, age?: number } = {
+//     name: 'Oleg',
+//     email: 'oleg44@gmail.com',
+//     // age: 28,
+// }
+// console.log(user2);
 //Проверяем свойства обекта
-if ('test' in user2) {
-    console.log(1);
-}
-else {
-    console.log(0);
-}
+// if ('test' in user2) {
+//     console.log(1);
+// } else {
+//     console.log(0);
+// }
 //Обрашение к необязательному свойству 
-console.log(user2.age);
+// console.log(user2.age);
 //Обект как аргумент функци 
-function userPorcess(some_var) {
-    console.log(some_var.email);
-}
-const obj = { name: 'Ivan', email: 'ivan@us.ua', 'test': false };
-userPorcess(obj);
+// function userPorcess(some_var: { name: string, email: string }): void {
+//     console.log(some_var.email);
+// }
+// const obj = { name: 'Ivan', email: 'ivan@us.ua', 'test': false };
+// userPorcess(obj)
 //Декомпазиция
-function foo({ name, email }) {
-    console.log(name, email);
+// function foo({ name, email }: { name: string, email: string }) {
+//     console.log(name, email);
+// }
+// foo(user2)
+//OOP
+//Обявляем класс
+class Button {
+    text;
+    className;
 }
-foo(user2);
+const b_01 = new Button();
+b_01.text = 'Push me';
+b_01.className = ['btn', 'btn-success'];
+console.log(b_01);
+//Заносим данные сразу в класс 
+class Heade {
+    text = 'Header-1 ';
+    fontZise = '24px';
+}
+const h_01 = new Heade();
+console.log(h_01);
+//Методы 
+class Btn {
+    text;
+    className;
+    create() {
+        const button = document.createElement('button');
+        button.textContent = this.text;
+        this.className.forEach(item => button.classList.add(item));
+        return button;
+    }
+}
+const b_02 = new Btn();
+b_02.text = 'button';
+b_02.className = ['btn', 'btn-success'];
+document.body.append(b_02.create());
+//Конструкот 
+class ButtonElement {
+    text;
+    className;
+    role;
+    constructor(text, className) {
+        this.text = text;
+        this.className = className;
+    }
+    create() {
+        const button = document.createElement('button');
+        button.textContent = this.text;
+        this.className.forEach(item => button.classList.add(item));
+        return button;
+    }
+}
+const b_03 = new ButtonElement('push me', ['btn', 'btn-primary']);
+document.body.append(b_03.create());
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -1,39 +1,102 @@
-const user: { name: string, email: string } = {
-    name: 'pupkin',
-    email: 'pupkin@gmail.com'
-}
-console.log(user);
+// const user: { name: string, email: string } = {
+//     name: 'pupkin',
+//     email: 'pupkin@gmail.com'
+// }
+// console.log(user);
 
 //Необязательные свойства
-const user2: { name: string, email: string, age?: number } = {
-    name: 'Oleg',
-    email: 'oleg44@gmail.com',
-    // age: 28,
-}
-console.log(user2);
+// const user2: { name: string, email: string, age?: number } = {
+//     name: 'Oleg',
+//     email: 'oleg44@gmail.com',
+//     // age: 28,
+// }
+// console.log(user2);
 
 
 //Проверяем свойства обекта
-if ('test' in user2) {
-    console.log(1);
-} else {
-    console.log(0);
-}
+// if ('test' in user2) {
+//     console.log(1);
+// } else {
+//     console.log(0);
+// }
 
 //Обрашение к необязательному свойству 
-console.log(user2.age);
+// console.log(user2.age);
 
 //Обект как аргумент функци 
-function userPorcess(some_var: { name: string, email: string }): void {
-    console.log(some_var.email);
-}
+// function userPorcess(some_var: { name: string, email: string }): void {
+//     console.log(some_var.email);
+// }
 
-const obj = { name: 'Ivan', email: 'ivan@us.ua', 'test': false };
-userPorcess(obj)
+// const obj = { name: 'Ivan', email: 'ivan@us.ua', 'test': false };
+// userPorcess(obj)
 
 //Декомпазиция
 
-function foo({ name, email }: { name: string, email: string }) {
-    console.log(name, email);
+// function foo({ name, email }: { name: string, email: string }) {
+//     console.log(name, email);
+// }
+// foo(user2)
+
+//OOP
+//Обявляем класс
+class Button {
+    text: string;
+    className: string[];
 }
-foo(user2)
+
+const b_01 = new Button();
+b_01.text = 'Push me';
+b_01.className = ['btn', 'btn-success'];
+console.log(b_01);
+
+//Заносим данные сразу в класс 
+
+class Heade {
+    text: string = 'Header-1 ';
+    fontZise: string = '24px';
+
+}
+const h_01 = new Heade();
+console.log(h_01);
+
+//Методы 
+
+class Btn {
+    text: string;
+    className: string[];
+    create(): HTMLButtonElement {
+        const button = document.createElement('button');
+        button.textContent = this.text;
+        this.className.forEach(item => button.classList.add(item));
+        return button;
+    }
+}
+
+const b_02 = new Btn();
+b_02.text = 'button';
+b_02.className = ['btn', 'btn-success'];
+
+document.body.append(b_02.create())
+
+//Конструкот 
+
+class ButtonElement {
+    text: string;
+    className: string[];
+    readonly role: string;
+    constructor(text: string, className: string[]) {
+        this.text = text;
+        this.className = className;
+    }
+
+    create(): HTMLButtonElement {
+        const button = document.createElement('button');
+        button.textContent = this.text;
+        this.className.forEach(item => button.classList.add(item));
+        return button;
+    }
+}
+
+const b_03 = new ButtonElement('push me', ['btn', 'btn-primary']);
+document.body.append(b_03.create())
